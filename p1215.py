c115fad4201cb7c80c0bdd6d0249ee689f308c62
lpe110 <lpe110@126.com>
2017-12-20 15:08:53 +0800

    write my first file

---
+++ p1215.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+from collections import deque
+from collections import defaultdict
+
+q=deque(['a','b','c','d'])
+
+q.append('M')
+q.appendleft('L')
+print(q[3])
+
+Point=namedtuple('Point21M',('x','y'))
+p=Point(2,6)
+print(p.x)
+
+dd=defaultdict(lambda: 'N/A')
+#dd={'k1':90,'k2':21,'k3':'mpt8'}
+print(dd['key2'])
